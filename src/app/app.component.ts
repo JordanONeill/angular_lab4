650fa07e42b4d3140df82e70341a6ae033a78c03
Jordan <G00416446@atu.ie>
2025-03-02 23:12:20 +0000

    First Commit for Part 1

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  count: number = 0;
+  showMessage: boolean = false;
+
+  incrementCount() {
+    this.count++;
+  }
+
+  toggleMessage() {
+    this.showMessage = !this.showMessage;
+  }
+}
